6e5f679cd2ee27e97c24096ec7f45aed32e8f72b
Novelia Arum <noveliaarum@gmail.com>
2023-10-28 22:32:50 +0700

    Update Web Portfolio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+//smooth scrolling & offset saat navbar di klik
+$('.nav-link').on('click', function(e) {
+    // ambil isi href
+    var section = $(this).attr('href');
+    //ambil elemen tsb
+    var link = $(section);
+    console.log($('body').scrollTop());
+    //pindahkan scroll
+   $('html, body').animate ({
+        scrollTop: link.offset().top - 100
+    }, 1000);
+
+   e.preventDefault();
+});
+
+// munculkan background navbar ketika di scroll
 $(window).scroll(function() {
     let scroll = $(window).scrollTop();
-    if (scroll >= 200) {
+    if (scroll >= 100) {
         $(".navbar").css("background", "white");
         $(".navbar").css("transition", "0.2s ease-out");
         $(".navbar").addClass("shadow-sm");
